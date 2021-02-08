d517662baf03bcbc73a8cfa62510ed3c1d1db169
Edgar MAURANNE <hiroedg@gmail.com>
2021-02-08 12:04:52 +0100

    OrgaController

---
+++ app/controllers/OrgaController.php
@@ -0,0 +1,15 @@
+<?php
+namespace controllers;
+ use models\Organization;
+
+ /**
+  * Controller OrgaController
+  */
+class OrgaController extends ControllerBase{
+    #[Route('orga')]
+	public function index(){
+		$orgas=DAO::getAll(Organization::class,"",['groupes']);
+		var_dump($orgas);
+		$this->loadView("OrgaController/index.html",['orgas'=>$orgas]);
+	}
+}
