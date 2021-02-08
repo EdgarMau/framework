5413b77e6a95e4df45a53275c342b4c19f2d6a75
Edgar MAURANNE <hiroedg@gmail.com>
2021-02-08 12:18:18 +0100

    to_string

--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -8,7 +8,7 @@ namespace controllers;
 class OrgaController extends ControllerBase{
     #[Route('orga')]
 	public function index(){
-		$orgas=DAO::getAll(Organization::class,"",['groupes']);
+		$orgas=DAO::getAll(Organization::class,"",['groupes.users']);
 		var_dump($orgas);
 		$this->loadView("OrgaController/index.html",['orgas'=>$orgas]);
 	}
