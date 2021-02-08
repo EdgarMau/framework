1bcbf454b75703f22a53331177fdeb88a48fc0bd
Edgar MAURANNE <hiroedg@gmail.com>
2021-02-08 12:52:16 +0100

    ajout de diverses fonctions

---
+++ app/cache/controllers/routes.default.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("/orga/"=>["controller"=>"controllers\\OrgaController","action"=>"index","parameters"=>[],"name"=>"orga.index","cache"=>false,"duration"=>0],"/orga/(.+?)/"=>["controller"=>"controllers\\OrgaController","action"=>"getOne","parameters"=>[0],"name"=>"orga.getOne","cache"=>false,"duration"=>0]);
--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -1,15 +1,25 @@
 <?php
 namespace controllers;
- use models\Organization;
+use models\Organization;
+use Ubiquity\attributes\items\router\Route;
+use Ubiquity\orm\DAO;
 
- /**
-  * Controller OrgaController
-  */
+/**
+ * Controller OrgaController
+ */
 class OrgaController extends ControllerBase{
-    #[Route('orga')]
-	public function index(){
-		$orgas=DAO::getAll(Organization::class,"",['groupes.users']);
-		var_dump($orgas);
-		$this->loadView("OrgaController/index.html",['orgas'=>$orgas]);
-	}
+
+    #[Route(path: "orga",name: "orga.index")]
+    public function index(){
+        $orgas=DAO::getAll(Organization::class, "",false);
+        $this->loadView("OrgaController/index.html",['orgas'=>$orgas]);
+    }
+
+
+    #[Route(path: "orga/{idOrga}",name: "orga.getOne")]
+    public function getOne($idOrga){
+        $orga=DAO::getById(Organization::class, $idOrga,['groupe.users']);
+        $this->loadDefaultView(['orga'=>$orga]);
+    }
+
 }
