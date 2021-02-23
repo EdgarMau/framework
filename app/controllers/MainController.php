de726d3bf6a09ec049491e6ead55a42bdcc31e72
Edgar MAURANNE <hiroedg@gmail.com>
2021-02-23 15:59:29 +0100

    Ajout dans views/MainController de listGroup et OrgaForm, ajout dans MainController de orgaForm,addOrga,ListGroups

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,17 +1,27 @@
 <?php
 namespace controllers;
 
+use models\Group;
+use models\Organization;
 use models\User;
+use services\dao\OrgaRepository;
+use services\ui\UIGroups;
+use Ubiquity\attributes\items\di\Autowired;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
 use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\USession;
 
 /**
  * Controller MainController
  */
 class MainController extends ControllerBase{
     use WithAuthTrait;
+
+    private $ui;
+    private $uiService;
+
     #[Route('_default', name:'home')]
     public function index(){
         $this->jquery->getHref('a[data-target]', parameters:['historize'=>false,
@@ -42,5 +52,31 @@ class MainController extends ControllerBase{
         $this->ui->newUser('frm-user');
         $this->jquery->renderView('main/vForm.html');
     }
+    #[Autowired]
+    private OrgaRepository $repo;
 
+    public function setRepo(OrgaRepository $repo): void {
+        $this->repo = $repo;
+    }
+    public function initialize() {
+        $this->ui=new UIGroups($this);
+        parent::initialize();
+    }
+    #[Route('groups/list',name: 'groups.list')]
+    public function listGroups(){
+    $idOrga=USession::get('idOrga');
+    $groups=DAO::getAll(Group::class,'idOrganization= ?',false,[$idOrga]);
+    $this->uiService->listGroups($groups);
+    $this->jquery->renderDefaultView();
+}
+    #[Get('newOrga',name: 'newOrga')]
+    public function orgaForm() {
+    $this->uiService->orgaForm(new Organization());
+    $this->jquery->renderDefaultView();
+}
+
+    #[Post('addOrga', name:'addOrga')]
+    public function addOrga() {
+    var_dump($_POST);
+    }
 }
